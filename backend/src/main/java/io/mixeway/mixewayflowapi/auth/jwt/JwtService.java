5f2dd72eb49b40094fcfe50fb55461d30d82cfcd
Tomek <tomasz.bondaruk@wp.pl>
2026-01-06 13:35:24 +0100

    Initial implementation of CI/CD - back-end

--- backend/src/main/java/io/mixeway/mixewayflowapi/auth/jwt/JwtService.java
+++ backend/src/main/java/io/mixeway/mixewayflowapi/auth/jwt/JwtService.java
@@ -3,13 +3,11 @@ package io.mixeway.mixewayflowapi.auth.jwt;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
-import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -17,6 +15,7 @@ import java.util.function.Function;
 
 @Component
 public class JwtService {
+
 
     private static final SecretKey SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS256);
 
