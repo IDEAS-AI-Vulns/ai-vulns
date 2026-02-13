5f5882707b35193c143551d9e6ba19072836a133
Tomek <tomasz.bondaruk@wp.pl>
2026-02-13 14:47:37 +0100

    Update of the workflow

--- frontend/src/app/directives/hasRole/has-role.directive.spec.ts
+++ frontend/src/app/directives/hasRole/has-role.directive.spec.ts
@@ -17,6 +17,7 @@ class TestComponent {
     requiredRole: string | string[] = 'ADMIN';
 }
 
+
 describe('HasRoleDirective', () => {
     let component: TestComponent;
     let fixture: ComponentFixture<TestComponent>;
